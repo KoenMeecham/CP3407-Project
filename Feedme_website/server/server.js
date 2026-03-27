f749cd233545881e0d49380ef966f01449bc3a1d
Koen Meecham <koen.meecham@my.jcu.edu.au>
2026-03-27 17:42:58 +1000

    update server.js

--- Feedme_website/server/server.js
+++ Feedme_website/server/server.js
@@ -15,8 +15,9 @@ const jwksRsa = require("jwks-rsa");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
 
 /* =========================
    COGNITO JWT MIDDLEWARE
@@ -78,6 +79,3 @@ app.use("/api", (req, res) => {
   res.status(404).json({ error: "API route not found" });
 });
 
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
