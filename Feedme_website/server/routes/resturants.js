13028193c14e075a63b173d5948788031a75c6b0
Koen Meecham <koen.meecham@my.jcu.edu.au>
2026-03-27 16:09:59 +1000

    fix require database.js

--- Feedme_website/server/routes/resturants.js
+++ Feedme_website/server/routes/resturants.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../db");
+const db = require("../database");
 
 function parsePositiveInt(value, fallback) {
   const parsed = parseInt(value, 10);
